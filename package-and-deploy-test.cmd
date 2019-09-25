f2c55a468b847a099c06d8c154c4218a9e340dbe
manjime83 <man.jime@gmail.com>
2019-09-25 11:43:59 -0500

    api modifications

--- package-and-deploy-test.cmd
+++ package-and-deploy-test.cmd
@@ -1,2 +1,3 @@
  call aws cloudformation package --template-file mipaquete.yaml --output-template-file mipaquete-packaged.yaml --s3-bucket transadmin.co-cf-templates
- call aws cloudformation deploy --template-file mipaquete-packaged.yaml --stack-name mipaquete-test --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ProjectName=mipaquete EnvType=test Domain=test.mipaquete.transadmin.co
+ call aws cloudformation deploy --template-file mipaquete-packaged.yaml --s3-bucket transadmin.co-cf-templates --stack-name mipaquete-test --capabilities CAPABILITY_NAMED_IAM --parameter-overrides ProjectName=mipaquete EnvType=test Domain=test.mipaquete.transadmin.co
+ 
