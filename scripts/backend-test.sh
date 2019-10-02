982718e8df7aeb2148190fd8439de618f1ef2b14
Manuel Jiménez <man.jime@gmail.com>
2019-10-01 20:14:09 -0500

    ajustes al api

--- scripts/backend-test.sh
+++ scripts/backend-test.sh
@@ -4,4 +4,4 @@ cd ../../backend
 docker build -t 948003242781.dkr.ecr.us-east-1.amazonaws.com/mipaquete-test/backend .
 eval $(aws ecr get-login --no-include-email --region us-east-1 --registry-ids 948003242781 --profile mipaquete-test)
 docker push 948003242781.dkr.ecr.us-east-1.amazonaws.com/mipaquete-test/backend
-aws ecs update-service --cluster mipaquete-test-cluster --service mipaquete-test-service --force-new-deployment
+aws ecs update-service --cluster mipaquete-test-cluster --service mipaquete-test-service --force-new-deployment --profile mipaquete-test
