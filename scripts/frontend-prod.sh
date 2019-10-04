16f803e7148cc5624c4b8ce2183eb43901234ba7
Manuel Jiménez <man.jime@gmail.com>
2019-10-04 20:57:37 +0000

    cambios al front

--- scripts/frontend-prod.sh
+++ scripts/frontend-prod.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 cd ../../frontend
-npm install --prod
-npm run build
+npm install
+NODE_OPTIONS='--max-old-space-size=4096' npm run build
 aws s3 sync dist s3://app.mipaquete.transadmin.co --delete --profile mipaquete-prod
-aws cloudfront create-invalidation --distribution-id E1V3HVTXFWYGJM --paths "/*" --profile mipaquete-prod
+aws cloudfront create-invalidation --distribution-id E3UL4M7ZLV1RBR --paths "/*" --profile mipaquete-prod
