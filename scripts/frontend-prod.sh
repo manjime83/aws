8c197123f5f543403a6273113deb0c7912cc51dd
Manuel Jiménez <man.jime@gmail.com>
2019-10-08 18:14:48 +0000

    reubicacion de certificados temporales

--- scripts/frontend-prod.sh
+++ scripts/frontend-prod.sh
@@ -3,5 +3,5 @@
 cd ../../frontend
 npm install
 NODE_OPTIONS='--max-old-space-size=4096' npm run build
-aws s3 sync dist s3://app.mipaquete.transadmin.co --delete --profile mipaquete-prod
+aws s3 sync dist s3://app.mipaquete.com --delete --profile mipaquete-prod
 aws cloudfront create-invalidation --distribution-id E3UL4M7ZLV1RBR --paths "/*" --profile mipaquete-prod
--- scripts/frontend-test.sh
+++ scripts/frontend-test.sh
@@ -3,4 +3,4 @@
 cd ../../frontend
 npm install
 NODE_OPTIONS='--max-old-space-size=4096' npm run build:test
-aws s3 sync dist s3://app.test.mipaquete.transadmin.co --delete --profile mipaquete-test
+aws s3 sync dist s3://app.test.mipaquete.com --delete --profile mipaquete-test
