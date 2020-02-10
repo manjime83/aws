2067cb36f6987e71f19531fad9d1f8c7a24d158e
Manuel Jiménez <man.jime@gmail.com>
2020-02-10 02:24:10 -0500

    cambio en los tamaños de los recursos

--- cfn-prod.sh
+++ cfn-prod.sh
@@ -6,4 +6,4 @@ aws cloudformation deploy --template-file packaged.yaml --stack-name mipaquete-p
     EnvType=prod \
     Domain=mipaquete.com \
     SSLCertificate=arn:aws:acm:us-east-1:468253634394:certificate/21ddc356-2468-4271-b19d-775f3cf5a9b9 \
-    MinContainers=2 MaxContainers=4
+    MinContainers=2 MaxContainers=3
